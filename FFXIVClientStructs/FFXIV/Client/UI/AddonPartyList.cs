7de7e9227652679bee693b39a577c67bb05259b6
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-11-15 11:57:05 -0600

    update PartyListMemberStruct

--- FFXIVClientStructs/FFXIV/Client/UI/AddonPartyList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonPartyList.cs
@@ -45,28 +45,29 @@ public unsafe partial struct AddonPartyList {
     [FieldOffset(0x13F3)] public byte ChocoboCount; // or ChocoboSummoned?
 
     [GenerateInterop]
-    [StructLayout(LayoutKind.Explicit, Size = 0xF8)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x100)]
     public partial struct PartyListMemberStruct {
         [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray10<Pointer<AtkComponentIconText>> _statusIcons;
         [FieldOffset(0x50)] public AtkComponentBase* PartyMemberComponent;
         [FieldOffset(0x58)] public AtkTextNode* IconBottomLeftText;
         [FieldOffset(0x60)] public AtkResNode* NameAndBarsContainer;
-        [FieldOffset(0x68)] public AtkTextNode* GroupSlotIndicator;
-        [FieldOffset(0x70)] public AtkTextNode* Name;
-        [FieldOffset(0x78)] public AtkTextNode* CastingActionName;
-        [FieldOffset(0x80)] public AtkImageNode* CastingProgressBar;
-        [FieldOffset(0x88)] public AtkImageNode* CastingProgressBarBackground;
-        [FieldOffset(0x90)] public AtkResNode* EmnityBarContainer;
-        [FieldOffset(0x98)] public AtkNineGridNode* EmnityBarFill;
-        [FieldOffset(0xA0)] public AtkImageNode* ClassJobIcon;
-        [FieldOffset(0xA8)] public void* UnknownA8;
-        [FieldOffset(0xB0)] public AtkImageNode* UnknownImageB0;
-        [FieldOffset(0xB8)] public AtkComponentBase* HPGaugeComponent;
-        [FieldOffset(0xC0)] public AtkComponentGaugeBar* HPGaugeBar;
-        [FieldOffset(0xC8)] public AtkComponentGaugeBar* MPGaugeBar;
-        [FieldOffset(0xD0)] public AtkResNode* TargetGlowContainer;
-        [FieldOffset(0xD8)] public AtkNineGridNode* ClickFlash;
-        [FieldOffset(0xE0)] public AtkNineGridNode* TargetGlow;
+        [FieldOffset(0x68)] public AtkResNode* Unknown68;  // seems to be related to MPGaugeBar
+        [FieldOffset(0x70)] public AtkTextNode* GroupSlotIndicator;
+        [FieldOffset(0x78)] public AtkTextNode* Name;
+        [FieldOffset(0x80)] public AtkTextNode* CastingActionName;
+        [FieldOffset(0x88)] public AtkImageNode* CastingProgressBar;
+        [FieldOffset(0x90)] public AtkImageNode* CastingProgressBarBackground;
+        [FieldOffset(0x98)] public AtkResNode* EmnityBarContainer;
+        [FieldOffset(0xA0)] public AtkNineGridNode* EmnityBarFill;
+        [FieldOffset(0xA8)] public AtkImageNode* ClassJobIcon;
+        [FieldOffset(0xB0)] public void* UnknownB0;
+        [FieldOffset(0xB8)] public AtkImageNode* UnknownImageB8;
+        [FieldOffset(0xC0)] public AtkComponentBase* HPGaugeComponent;
+        [FieldOffset(0xC8)] public AtkComponentGaugeBar* HPGaugeBar;
+        [FieldOffset(0xD0)] public AtkComponentGaugeBar* MPGaugeBar;
+        [FieldOffset(0xD8)] public AtkResNode* TargetGlowContainer;
+        [FieldOffset(0xE0)] public AtkNineGridNode* ClickFlash;
+        [FieldOffset(0xF0)] public AtkNineGridNode* TargetGlow;
         [FieldOffset(0xF0)] public byte EmnityByte; //01 or 02 or FF
     }
 }
